ec21c9a118eeac32a8895979ac76ae1078422533
Charandeep <ckaunsel123@gmail.com>
2021-07-31 22:57:03 +0530

    update

--- reels/src/Login.jsx
+++ reels/src/Login.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { Redirect } from "react-router-dom";
 import { auth, signInWithGoogle } from "./firebase";
 
 let Login = (props) => {
@@ -6,29 +7,34 @@ let Login = (props) => {
     useEffect(() => {
 
         // jaab user login hota hai yaa logout hota hai tho yaa run hota hai
-        auth.onAuthStateChanged( (user) => { 
+        auth.onAuthStateChanged((user) => {
             // if login -> user info
             // if logout -> user = null
-            if(user){
-                let {displayName, email } = user; // destructuring kar raha hai
+            if (user) {
+                let { displayName, email } = user; // destructuring kar raha hai
                 console.log(user);
-                
-                props.handlerUser({ displayName, email});
+
+                props.handlerUser({ displayName, email });
             } else {
                 props.handlerUser(user);
             }
             // console.log(user);
-        }); 
+        });
     }, []);
 
     return (
-        <button
-            type="submit"
-            onClick={signInWithGoogle}
-            className="btn btn-primary m-4"
-        >
-            Login With Google
-        </button>
+        <div>
+            {props.user ? <Redirect to="/home" /> : ""}
+
+            <button
+                type="submit"
+                onClick={signInWithGoogle}
+                className="btn btn-primary m-4"
+            >
+                Login With Google
+            </button>
+
+        </div>
     );
 };
 
